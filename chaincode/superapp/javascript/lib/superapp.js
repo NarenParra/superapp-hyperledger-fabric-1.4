44e82f53ca1dccc92b5618fd73b8a6eb6d1995bb
Naren Felipe Parra Trujillo - Ceiba Software <naren.parra@ceiba.com.co>
2021-06-11 10:35:57 -0500

    chain code superapp

--- chaincode/superapp/javascript/lib/superapp.js
+++ chaincode/superapp/javascript/lib/superapp.js
@@ -174,9 +174,52 @@ class SuperApp extends Contract {
 
         let resultsIterator = await ctx.stub.getHistoryForKey(id);
         //let method = thisClass["getAllResults"];
-        // let results = await method(resultsIterator, true);
+        let results = await getAllResults(resultsIterator, true);
+        console.log(Buffer.from(JSON.stringify(results)));
+        return Buffer.from(JSON.stringify(results));
+    }
 
-        return console.log(Buffer.from(JSON.stringify(resultsIterator)));
+    async getAllResults(iterator, isHistory) {
+        let allResults = [];
+        while (true) {
+            let res = await iterator.next();
+
+            if (res.value && res.value.value.toString()) {
+                let jsonRes = {};
+                console.log(res.value.value.toString("utf8"));
+
+                if (isHistory && isHistory === true) {
+                    jsonRes.TxId = res.value.tx_id;
+                    jsonRes.Timestamp = res.value.timestamp;
+                    jsonRes.IsDelete = res.value.is_delete.toString();
+                    try {
+                        jsonRes.Value = JSON.parse(
+                            res.value.value.toString("utf8")
+                        );
+                    } catch (err) {
+                        console.log(err);
+                        jsonRes.Value = res.value.value.toString("utf8");
+                    }
+                } else {
+                    jsonRes.Key = res.value.key;
+                    try {
+                        jsonRes.Record = JSON.parse(
+                            res.value.value.toString("utf8")
+                        );
+                    } catch (err) {
+                        console.log(err);
+                        jsonRes.Record = res.value.value.toString("utf8");
+                    }
+                }
+                allResults.push(jsonRes);
+            }
+            if (res.done) {
+                console.log("end of data");
+                await iterator.close();
+                console.info(allResults);
+                return allResults;
+            }
+        }
     }
 }
 
